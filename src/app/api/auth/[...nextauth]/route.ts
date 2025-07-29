626527afbabbb8f9356c604f41613fff7afe2b3b
Natnael <natnael.tesfaye2022@gmail.com>
2025-07-29 17:36:20 +0300

    NextAuth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,91 @@
 import NextAuth from 'next-auth';
-import Credentials, { CredentialsProvider } from 'next-auth/providers/credentials';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
+import bcrypt from 'bcryptjs';
+import { pages } from 'next/dist/build/templates/app-page';
+import { signIn } from 'next-auth/react';
+
+import { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    }
+  }
+  interface User extends DefaultUser {
+    id: string;
+  }
+}
 
 const handler = NextAuth({
-  providers: [
+    adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: 'jwt',
+    },
+    providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
     CredentialsProvider({
         name: "Credentials",
         credentials: {
-            email: { label:"email", type:"text", placeholder: "Enter your email" },
-            password: { label:"password", type:"password", placeholder: "Enter your password"}
+            email: { label: "Email", type: "text", placeholder: "Enter your email" },
+            password: { label: "Password", type: "password", placeholder: "Enter your password" },
+        },
+
+        async authorize(credentials) {
+            if(!credentials?.email || !credentials?.password) {
+                return null;
+            }
+
+            const user = await prisma.user.findUnique({
+                where: { email: credentials.email as string },
+            });
+
+            if(!user || !user.password) {
+                return null;
+            }
+
+            const isPasswordValid = await bcrypt.compare(credentials.password as string, user.password);
+            if(!isPasswordValid) {
+                return null;
+            }
+
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    image: user.image || null,
+                };
+            },
+        }),
+    ],
+
+    pages:{
+        signIn: '/login',
     },
-    async authorize(credentials, req){
-        
-    }
-)
+
+    callbacks: {
+        async session({ session, token }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.sub = user.id;
+            }
+            return token;
+        },
+    },
+});
+
+export { handler as GET, handler as POST };
